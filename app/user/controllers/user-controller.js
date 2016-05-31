9a1f366905f48d4c4ad9c419000187f299804894
phipex <aphipex@gmail.com>
2016-05-31 13:08:25 -0500

    feature: agregada logica que condinciona la navegacion, solo se agrega, no se ha implementado

--- app/user/controllers/user-controller.js
+++ app/user/controllers/user-controller.js
@@ -12,8 +12,25 @@
     .module('user')
     .controller('UserCtrl', UserCtrl);
 
-  function UserCtrl() {
+  UserCtrl.$inject = ['Authentification'];
+
+  function UserCtrl(Authentification) {
     var vm = this;
     vm.ctrlName = 'UserCtrl';
+
+    //TODO verificar si el usuario esta logeado
+    var isLogin = Authentification.isLogin();
+
+    if(isLogin){
+
+    }
+
+    //TODO solicitar los recursos para el usuario
+    function requestAllowRecurses() {
+
+    }
+
+    //TODO mostrar un menu con los servicios del usuario
+
   }
 }());
