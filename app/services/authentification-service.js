b021c0d98ff127a77632e41cf99c3ef70a20a0b5
phipex <aphipex@gmail.com>
2016-05-31 13:07:46 -0500

    add: agregar nuevas funciones sin implementar

--- app/services/authentification-service.js
+++ app/services/authentification-service.js
@@ -5,7 +5,7 @@
    * @ngdoc service
    * @name bingo.service:Authentification
    *
-   * @description servicio encargado de gestionar el login
+   * @description servicio encargado de gestionar el login y la session localmente
    *
    */
   angular
@@ -13,6 +13,8 @@
     .service('Authentification', Authentification);
 
   function Authentification() {
+    // TODO cambiar de diseño
+
     /* var self = this;
 
     self.get = function () {
@@ -23,27 +25,52 @@
      *
      */
     return {
+
       get: get,
       login: login,
       logout: logout,
-      isLogin: isLogin
+      isLogin: isLogin,
+      getAllowedRecurses: getAllowedRecurses
     };
 
-    function get() {
+    /**
+     * @deprecated solo para test
+     * @returns {string} nombre del servicio
+       */
+    function get(){
       return 'Authentification';
     }
 
+    /**
+     * autentifica el usuario
+      * @param userName nombre del usuario
+     * @param password password del usuario
+     * @return {promise} promesa con la identificacion del usuario
+       */
     function login(userName, password) {
 
     }
 
+    /**
+     * cierra la session del usuario
+     *
+     */
     function logout() {
 
     }
 
+    /**
+     * infica si el usuario esta logeado segun la configuracion local
+     */
     function isLogin() {
 
     }
 
+      /**
+       * Retorna los recursos permitidos para el rol del usuario actual
+       */
+    function getAllowedRecurses() {
+
+    }
   }
 }());
